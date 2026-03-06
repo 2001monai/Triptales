4a0eab67172e64846b415c40a78e2ec2f56c1a21
2001monai <20017ananya@gmail.com>
2026-03-07 00:11:12 +0600

    Apply .gitignore and remove unwanted files from tracking

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,22 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import dns from "dns";
 
-const app = express()
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+dotenv.config();
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI) 
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error:", err);
+  });
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000!")
-})
+  console.log("Server is running on port 3000!");
+});
